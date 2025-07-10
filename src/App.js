c59c7a432401832076294b7448bb8929ce463d6f
canberksamur <canberksamur@icloud.com>
2025-07-10 22:01:14 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 32, fontFamily: "sans-serif" }}>
+      <h1>Lost Connections</h1>
+      <p>
+        Welcome! This is a simple app to help people share and find lost connections.<br />
+        Start building your app by editing <code>src/App.js</code>.
+      </p>
+    </div>
+  );
+}
+
+export default App;
